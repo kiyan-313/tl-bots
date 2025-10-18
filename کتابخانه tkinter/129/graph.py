3590226fefd82b88519fb1b78028b6f841b06f22
kiyan-313 <samoraiivkiyan313@gmail.com>
2025-10-18 12:35:15 +0330

    Delete کتابخانه tkinter directory

--- کتابخانه tkinter/129/graph.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-window = Tk()
-window.title('instgram profile donloder')
-window.geometry("750x550")
-window.resizable(width=False, height=False)
-
-window.mainloop()
--- کتابخانه tkinter/129/main.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-
-window=Tk()
-
-window.mainloop()
