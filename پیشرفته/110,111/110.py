cbc08e575f55d887db48f3dcb18362188ff8d437
kiyan-313 <samoraiivkiyan313@gmail.com>
2025-10-18 12:34:58 +0330

    Delete پیشرفته directory

--- پیشرفته/110,111/110.py
+++
@@ -1,7 +0,0 @@
-mylist=['amir','ali','reza','farid'] #iterable
-
-myiterator=iter(mylist) #iterable  ===> iterator
-print(next(myiterator))
-print(next(myiterator))
-print(next(myiterator))
-print(next(myiterator))
--- پیشرفته/126/126.py
+++
@@ -1,8 +0,0 @@
-built in
-
-global
-
-local
-
-
-enclosed
